f2ee71dc641035d126ce910c24ff5641ad3c0ec4
claireWei <claire610323@gmail.com>
2022-08-21 17:07:37 +0800

    DropZone styles

---
+++ src/components/DropZone.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="dropzone page-flex">
+    <span>Drag File</span>
+    <span>OR</span>
+    <label for="dropzoneFile" class="cursor-pointer">Select File</label>
+    <input type="file" id="dropzoneFile" class="dropzoneFile" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "DropzoneComponent",
+};
+</script>
+
+<style lang="scss" scoped>
+@use "../styles/mixin/_colors.scss" as colors;
+
+.dropzone {
+  width: 360px;
+  height: 180px;
+  flex-direction: column;
+  row-gap: 16px;
+  border: 3px dashed colors.$success-primary;
+  background-color: colors.$white;
+  transition: 0.3s ease all;
+
+  label {
+    padding: 6px 9px;
+    color: colors.$white;
+    background-color: colors.$success-primary;
+    transition: 0.3s ease all;
+    border-radius: 5px;
+  }
+
+  input {
+    display: none;
+  }
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,40 @@
 <template>
-  <div class="home"></div>
+  <div class="home page-flex">
+    <h1>DropZone</h1>
+    <DropZone />
+    <span v-if="dropzoneFile" class="file-info"
+      >File: {{ dropzoneFile.name }}</span
+    >
+  </div>
 </template>
 
 <script>
+import DropZone from "../components/DropZone.vue";
+import { ref } from "vue";
+
 export default {
   name: "HomeView",
+  components: { DropZone },
+  setup() {
+    let dropzoneFile = ref("");
+
+    return { dropzoneFile };
+  },
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+@use "../styles/mixin/_colors.scss" as colors;
+
+.home {
+  flex-direction: column;
+  background-color: colors.$white;
+  h1 {
+    font-size: 36px;
+    margin-bottom: 24px;
+  }
+  .file-info {
+    margin-top: 18px;
+  }
+}
+</style>
